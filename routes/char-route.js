ef89ff144558d47f25f5c848f3d6a642e286ee38
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-03 17:49:12 -0300

    refactor (routes): update chars route payload construction

--- routes/char-route.js
+++ routes/char-route.js
@@ -1,13 +1,32 @@
 const chars = require('../models/char-model');
+const moves = require('../models/move-model');
+const types = require('../models/type-model');
 var express = require('express');
 var route = express.Router();
 
 route.get('/', (req, res) => {
-  res.json(chars);
+  var objects = chars.slice();
+  var newObjects = objects.map((value) => {
+    return deserialize(value);
+  }); 
+  res.json(newObjects);
 });
 
 route.get('/:id', (req, res) => {
-  res.json(chars[req.params.id]);
+  var object = chars[req.params.id];
+  var newObject = deserialize(object);
+  res.json(newObject);
 });
+
+function deserialize(object) {
+  var newObject = Object.assign({}, object);
+  newObject.moves = object.moves.map((value) => {
+    return moves[value];
+  });
+  newObject.types = object.types.map( (value) => { 
+    return types[value];
+  });
+  return newObject;
+}
 
 module.exports = route;
